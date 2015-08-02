b78af36b1519a803fe8ee6b717f2cddc4335a1fd
David-QuadCore <david.dupre@gmail.com>
2015-08-02 11:55:45 -0400

    Trying to get the body done
    Error as it stands now.

--- best.R
+++ best.R
@@ -2,66 +2,83 @@
 ## accepts two arguments the 2-character State and Outcome Name
 library(dplyr)
 library(tidyr)
- best <- function (state,oc){
-   
-   ## Read outcome data
-   
-   outcome <- read.csv("outcome-of-care-measures.csv",colClasses = "character")
-   
-   ## Check that the State and outcome are valid
-   
-   valid_states <- unique(outcome[,7])
-   
-   Hospitals <- outcome[,2]
-   
-   HA_counts <- outcome[,15]
-   HA_counts <- as.numeric(HA_counts)
-   
-   HF_counts <- outcome[,21]
-   HF_counts <- as.numeric(HF_counts)
-   
-   PN_counts <- outcome[,27]
-   PN_counts <- as.numeric(PN_counts)
-   
-   reportdata <- data.frame(HA_counts,HF_counts,PN_counts,Hospitals)
-   
-   valid_outcomes <- c("heart attack","pneumonia","heart failure")
-
-   notvalidST=FALSE
-   notvalidOUTCOME=FALSE
-
-   ## Check for valid states   
-   for (i in valid_states) {
-     if (i == state) {
-       notvalidST=TRUE
-       }
-   }
-  ## Check for valid Outcome
-   for (j in valid_outcomes) {
-     if (j == oc) {
-       notvalidOUTCOME = TRUE
-     }
-   }
-   
-   ## Disable Warning
-   options(warn=-1)
-  
-   ##   print(c("not valid =",notvalid))
-   
-   if (!notvalidST) {
-     stop("invalid state",call.=TRUE)
-   }
-   
-   if (!notvalidOUTCOME) {
-     stop("invalid outcome",call.=TRUE)
-   }
-   
-   ## Re-enable Warnings
-   options(warn=0)
-   
-   print("DID NOT STOP")
-   
-   ## Return hospital name in that state with lowest 30-day death rate
-   
-   
- }
+best <- function (state,oc){
+        
+        ## Read outcome data
+        outcome <- read.csv("outcome-of-care-measures.csv",colClasses = "character")
+        
+        ## Setup what is valid        
+        valid_states <- unique(outcome[,7])
+        valid_outcomes <- c("heart attack","pneumonia","heart failure")
+        
+        ## Get names of Hospitals
+        Hospitals <- outcome[,2]
+        States <- outcome[,7]
+        
+        ## Create new data frame of just completed rows of Hospitals and HA_counts
+        HA_counts <- outcome[,15]
+        HA_counts <- as.numeric(HA_counts)   
+        HA_counts_df <- data.frame(Hospitals,States,HA_counts)
+        good <- complete.cases(HA_counts_df)
+        HA_counts_df <- HA_counts_df[good,]
+        ##   HA_counts_min <- HA_counts_df()
+        
+        ## Create new data frame of just completed rows of Hospitals and HF_counts
+        
+        HF_counts <- outcome[,21]
+        HF_counts <- as.numeric(HF_counts)   
+        HF_counts_df <- data.frame(Hospitals,States,HF_counts)
+        good <- complete.cases(HF_counts_df)
+        HF_counts_df <- HF_counts_df[good,]
+        
+        
+        ## Create new data frame of just completed rows of Hospitals and PN_Counts
+        PN_counts <- outcome[,27]
+        PN_counts <- as.numeric(PN_counts)
+        PN_counts_df <- data.frame(Hospitals,States,PN_counts)
+        good <- complete.cases(PN_counts_df)
+        PN_counts_df <- PN_counts_df[good,]
+        
+        
+        reportdata <- data.frame(HA_counts,HF_counts,PN_counts,Hospitals,States)
+        
+        ## Check that the State and outcome are valid
+        notvalidST=FALSE
+        notvalidOUTCOME=FALSE
+        
+        ## Check for valid states   
+        for (i in valid_states) {
+                if (i == state) {
+                        notvalidST=TRUE
+                }
+        }
+        ## Check for valid Outcome
+        for (j in valid_outcomes) {
+                if (j == oc) {
+                        notvalidOUTCOME = TRUE
+                }
+        }
+        
+        ## Disable Warning
+        options(warn=-1)
+        
+        ##   print(c("not valid =",notvalid))
+        
+        if (!notvalidST) {
+                stop("invalid state",call.=TRUE)
+        }
+        
+        if (!notvalidOUTCOME) {
+                stop("invalid outcome",call.=TRUE)
+        }
+        
+        ## Re-enable Warnings
+        options(warn=0)
+        
+        ## Begin the processing of the Data Frames for each valid type
+        print("DID NOT STOP")
+        
+        ## Return hospital name in that state with lowest 30-day death rate
+        
+        
+}
