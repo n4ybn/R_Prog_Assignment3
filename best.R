345ecd2180c243541bdeca25be041417ee563b34
n4ybn-NewDell <david.dupre@gmail.com>
2015-08-03 08:40:56 -0400

    FinalUpdate
    Not completely working

--- best.R
+++ best.R
@@ -1,52 +1,20 @@
 ## Best function section 2 of assignment
 ## accepts two arguments the 2-character State and Outcome Name
-## library(dplyr)
+library(dplyr)
+library(sqldf)
 ## library(tidyr)
-rc <- setwd("C:/Downloads/Coursera/R_Prog_Assignment3")
+## rc <- setwd("C:/downloads/Coursera/R_Prog_Assignment3")
+rc <- setwd("C:/downloads/Coursera/R-Programming/R_Prog_Assignment3")
 
 best <- function (state,oc){
         
         ## Read outcome data
         outcome <- read.csv("outcome-of-care-measures.csv",colClasses = "character")
-        
+
         ## Setup what is valid        
         valid_states <- unique(outcome[,7])
         valid_outcomes <- c("heart attack","pneumonia","heart failure")
-        
-        ## Get names of Hospitals
-        Hospitals <- outcome[,2]
-        States <- outcome[,7]
-        
-        ## Create new data frame of just completed rows of Hospitals and HA_counts
-        HA_counts <- outcome[,15]
-        suppressWarnings(HA_counts <- as.numeric(HA_counts))   
-##        HA_counts_df <- data.frame(Hospitals,HA_counts)
-        HA_counts_df <- data.frame(Hospitals,States,HA_counts)
-        good <- complete.cases(HA_counts_df)
-        HA_counts_df <- HA_counts_df[good,]
-        ##   HA_counts_min <- HA_counts_df()
-        
-        ## Create new data frame of just completed rows of Hospitals and HF_counts
-        
-        HF_counts <- outcome[,21]
-        suppressWarnings(HF_counts <- as.numeric(HF_counts))   
-        HF_counts_df <- data.frame(Hospitals,States,HF_counts)
-    ##    HF_counts_df <- data.frame(Hospitals,HF_counts)
-        good <- complete.cases(HF_counts_df)
-        HF_counts_df <- HF_counts_df[good,]
-        
-        
-        ## Create new data frame of just completed rows of Hospitals and PN_Counts
-        PN_counts <- outcome[,27]
-        suppressWarnings(PN_counts <- as.numeric(PN_counts))
-        PN_counts_df <- data.frame(Hospitals,States,PN_counts)
-    ##  PN_counts_df <- data.frame(Hospitals,PN_counts)
-        good <- complete.cases(PN_counts_df)
-        PN_counts_df <- PN_counts_df[good,]
-        
-        
-        ## reportdata <- data.frame(HA_counts,HF_counts,PN_counts,Hospitals,States)
-        
+
         ## Check that the State and outcome are valid
         notvalidST=FALSE
         notvalidOUTCOME=FALSE
@@ -65,7 +33,7 @@ best <- function (state,oc){
         }
         
         ## Disable Warning
-         options(warn=-1)
+        options(warn=-1)
         
         ##   print(c("not valid =",notvalid))
         
@@ -78,10 +46,22 @@ best <- function (state,oc){
         }
         
         ## Re-enable Warnings
-        options(warn=0)
+        options(warn=0)        
+                
+        ## Get names of Hospitals
+        Hospitals <- outcome[,2]
+        States <- outcome[,7]
+        
+        ## Reduce rows to Single State
+        ocBS <- outcome[grep(state,outcome$State),]
+        
+        sorted_obs <- ocBS[with (ocBS, order(ocBS[,15],ocBS[,2])),]
+        print(sorted_obs[1,2])
+       
+
         
         ## Begin the processing of the Data Frames for each valid type
-        print("DID NOT STOP")
+       ## print("DID NOT STOP")
         
         ## Return hospital name in that state with lowest 30-day death rate
         
