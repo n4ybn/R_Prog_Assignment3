6aa2bbbf2aebb166ecc169f5613bc29ad3158aab
David-QuadCore <david.dupre@gmail.com>
2015-09-25 18:36:25 -0400

    how to start swirl

---
+++ swirl_how_to_Start.R
@@ -0,0 +1,18 @@
+install.packages("swirl")
+library(swirl)
+swirl()
+install_from_swirl("R Programming")
+## As of 2015-09-24
+## Course ID: rprog-032
+## Submission login (email): The email address associated with your Coursera account
+## Submission password: htBqWdP3VV
+
+
+## ------------- GETTING AND CLEANING Data!
+## Course ID: getdata-032
+## Submission login (email): david.dupre@gmail.com
+## Submission password: XVgXSXT372
+
+install_from_swirl("Getting and Cleaning Data")
+setwd("C:/Downloads/Coursera/getdata-032")
+swirl()
